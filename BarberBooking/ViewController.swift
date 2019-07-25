21873e46e854bb55477d147ab9a44ababd1f7e49
Nguyen Van Thang <nguyenvanthang@nguyens-macbook-pro.local>
2019-07-25 09:27:53 +0700

    Initial Commit

---
+++ BarberBooking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarberBooking
+//
+//  Created by Nguyen Van Thang on 7/25/19.
+//  Copyright © 2019 Nguyen Van Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
